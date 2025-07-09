eb1d6ae4260b28988f31f8bcafa0da93ad8549bd
AMAN <amankanojiya815@gmail.com>
2025-07-09 12:24:07 +0530

    feat: add server.js and dotenv configuration, update package.json dependencies

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+//server.js
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res)=>{
     res.send("Hello, World!");
 })
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port http://localhost:${PORT}`)
 })
