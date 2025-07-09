c602d8910419e77aa0f7a4e1e2356bfc88e0ced9
AMAN <amankanojiya815@gmail.com>
2025-07-09 13:04:43 +0530

    fix: downgrade dotenv version to 16.6.1 and add database connection logic

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import connectToDB from "./src/config/db.js";
 
 const app = express();
+app.use(express.json());
+connectToDB();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res)=>{
---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+async function connectToDB() {
+    try {
+        const { connection } = await mongoose.connect(process.env.MONGO_URL);
+
+        if (connection) {
+            console.log(`✅ Connected to MongoDB: ${connection.host}`);
+        }
+    } catch (error) {
+        console.log("❌ Error Connecting to DB: ", error);
+        process.exit(1);
+    }
+}
+export default connectToDB;
