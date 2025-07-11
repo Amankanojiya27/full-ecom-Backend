0f81456b99365f2e48911d6ef0e6b510812ea0a1
AMAN <amankanojiya815@gmail.com>
2025-07-11 11:44:57 +0530

    refactor: clean up commented-out code in verifyToken middleware for better readability

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,35 +1,6 @@
 import jwt from "jsonwebtoken";
-import User from "../auth/authModels.js"
+// import User from "../auth/authModels.js"
 
-// export const verifyToken = (req, res, next) => {
-//   const authHeader = req.headers.Authorization || req.headers.authorization;
-
-//   console.log("authHeader", authHeader)
-
-//   if (authHeader && authHeader.startsWith("Bearer")) {
-//     const token = authHeader.split(" ")[1];
-//     console.log("token",token)
-
-//     if (!token) {
-//       return res.status(401).json({
-//         message: "no token, authorization denied",
-//       });
-//     }
-//   }
-
-//   try {
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     console.log("decoded",decoded)
-//     req.user = decoded;
-//     console.log("The decoded user is : ", req.user);
-//     next();
-//   } catch (err) {
-//     res.status(400).json({
-//       message: " token is not valid",
-//     });
-//   }
-// };
 
 export const verifyToken = async (req, res, next) => {
   try {
